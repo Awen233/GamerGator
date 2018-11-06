7481d17df063a59eb252653190b870da4fbf662e
Adam Haubenstock <aehaubenstock.dev@gmail.com>
2018-11-06 09:18:17 -0500

    Add conditional rendering

--- client/js/controllers/home-events-controller.js
+++ client/js/controllers/home-events-controller.js
@@ -1,17 +1,26 @@
 angular.module('events').controller('HomeEventsController', ['$scope', 'Events', 
   function($scope, Events) {
     $scope.searchFields = Events.searchFields;
-    $scope.model = {
+    $scope.auth = {
+      username: 'a',
+      hash: 'a'
+    };
+    $scope.model = { // Also functions as a view model b/c of two-way binding
       searchParams: {
         title: '',
         host: '',
         categories: {}
       },
       allEvents: [],
-      shownEvents: []
+      shownEvents: [],
+      loggedIn: false
     };
+    // This is the only place where logged in should be set
+    $scope.$watch('auth', function() {
+      $scope.model.loggedIn = $scope.auth.username && $scope.auth.hash;
+    }, true);
     // This is the only place where shown events should be set
-    $scope.$watch('model', function () {
+    $scope.$watch('model', function() {
         // Filter all events using search params
         // Sort the events by most recent first
         var categories = []; // Iron the search categories dictionary out into a nice array
@@ -29,6 +38,7 @@ angular.module('events').controller('HomeEventsController', ['$scope', 'Events',
         // Check that the event name contains the search title
         .filter(event => event.event_name.toLowerCase().includes($scope.model.searchParams.title.toLowerCase()));
         // TODO: add the rest of the conditions
+        // TODO: sort by date
       }, true);
     // Do initial loading of information
     $scope.load = function() {
