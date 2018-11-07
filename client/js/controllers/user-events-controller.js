6ad739d363f9b9655dd420d6041e3c874cda06ae
ArielCoulson <ariel.coulson98@gmail.com>
2018-11-07 13:59:01 -0500

    Add controller

---
+++ client/js/controllers/user-events-controller.js
@@ -0,0 +1,21 @@
+angular.module('userevents').controller('UserEventsController', ['$scope', 'UserEvents', 
+  function($scope, Events) {
+    $scope.model = {
+      user_id: '',
+      events: []
+    };
+    // Do initial loading of information
+    $scope.load = function() {
+      UserEvents.api.getMy($scope.model.user_id).then(function(res) {
+        $scope.model.events = res.data;
+      }, function(error) {
+        console.log('Unable to retrieve my events:', error);
+      });
+    }
+    $scope.load();
+    // Route to single event page on click
+    $scope.eventWasClicked = function(id) {
+      console.log('Event clicked with id: ' + id);
+    };
+  }
+]);
---
+++ client/js/factories/user-event-factory.js
@@ -0,0 +1,11 @@
+angular.module('userevents', []).factory('UserEvents', function($http) {
+  const apiBase = 'http://localhost:8080/api';
+  const api = {
+    getMy: function(user_id) {
+        return $http.get(apiBase + '/users/' + user_id + '/MyEvents');
+    },
+  };
+  return {
+    api: api,
+  };
+});
