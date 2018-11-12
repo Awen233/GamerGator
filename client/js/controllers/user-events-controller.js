8c9c9f55078055cf8f10e79ee3560637d109e36e
ArielCoulson <ariel.coulson98@gmail.com>
2018-11-12 14:20:16 -0500

    linked & redirected pages

--- client/js/controllers/user-events-controller.js
+++ client/js/controllers/user-events-controller.js
@@ -1,9 +1,10 @@
-angular.module('userevents').controller('UserEventsController', ['$scope', 'UserEvents', $cookies,  
-  function($scope, UserEvents, $cookies) {
+angular.module('userevents').controller('UserEventsController', ['$scope', '$window', 'UserEvents', '$cookies',  
+  function($scope, $window, UserEvents, $cookies) {
     $scope.model = {
       user_id: '',
       events: []
     };
+
     // Do initial loading of information
     $scope.load = function() {
       UserEvents.api.getMy($scope.model.user_id).then(function(res) {
@@ -15,7 +16,10 @@ angular.module('userevents').controller('UserEventsController', ['$scope', 'User
     $scope.load();
     // Route to single event page on click
     $scope.eventWasClicked = function(id) {
+      $cookies.put('selectedEvent', id);
       console.log('Event clicked with id: ' + id);
+      $window.location.href = 'event.html';
+      console.log($cookies.getAll());
     };
   }
 ]);
