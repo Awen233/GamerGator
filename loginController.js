7885247abd644154df460fcb0e946a72bac73720
Jinhong Liu <jhlviking15@ufl.edu>
2018-11-12 17:35:28 -0500

    login page controller update

--- loginController.js
+++ loginController.js
@@ -1,11 +1,11 @@
-angular.module('users').controller('registerController', ['$scope', 'users',
+angular.module('users').controller('loginController', ['$scope', 'users',
     function ($scope, users) {
         $scope.users = users;
 
         $scope.addListing = function () {
             var update = {
-                user_id: $scope.newListing.user_id,
-                passWord: $scope.newListing.passWord,
+                user_id: $scope.users.user_id,
+                passWord: $scope.users.passWord,
             }
             $scope.users.push(update);
             $scope.users.user_id = '';
